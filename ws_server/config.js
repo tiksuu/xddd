beba4774229540baebb6e0fbeaf82280f29f1fd2
tiksuu <72652644+tiksuu@users.noreply.github.com>
2020-10-10 20:15:49 +0300

    Add files via upload

---
+++ ws_server/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  //-- [REQUIRED] IPv4 Address of your teamspeak 3 server
+  TSServer: "matala.prots33.xyz",
+
+  //-- [REQUIRED] Port of the ws_server
+  //-- Make sure you open the port you specify below
+  //-- Please use a port above 30k as some networks block those below it
+  WSServerPort: 30118,
+
+  //-- [OPTIONAL] IPv4 Address of the ws_server
+  //-- Set by autoconfig
+  // WSServerIP: "45.137.116.183",
+
+  //-- [OPTIONAL] Enable connection/disconnection logs
+  enableLogs: false,
+};
